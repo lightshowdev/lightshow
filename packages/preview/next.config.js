5b1c99c03ad83482f81677bbeb5a0e0b1ba66aad
Patrick Dillon <dillon76@gmail.com>
2022-04-14 20:44:05 -0400

    Add mui to preview app

--- packages/preview/next.config.js
+++ packages/preview/next.config.js
@@ -1,14 +1,3 @@
 module.exports = {
   basePath: '/preview',
-  // webpack: (config, { isServer }) => {
-  //   console.log(isServer);
-  //   // Fixes npm packages that depend on `fs` module
-  //   if (!isServer) {
-  //     config.resolve.alias = {
-  //       speaker: false,
-  //     };
-  //   }
-
-  //   return config;
-  // },
 };
--- packages/preview/src/pages/_app.tsx
+++ packages/preview/src/pages/_app.tsx
@@ -1,7 +1,31 @@
 import '../styles/global.css';
+import Head from 'next/head';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider } from '@emotion/react';
+import theme from '../theme';
+import createEmotionCache from '../helpers/createEmotionCache';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+function MyApp({
+  Component,
+  emotionCache = clientSideEmotionCache,
+  pageProps,
+}) {
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
 
 export default MyApp;

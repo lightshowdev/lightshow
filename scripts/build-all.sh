3c261cc108dadf174ba79ce617ddedf98f8f81d4
Patrick Dillon <dillon76@gmail.com>
2022-08-06 16:26:04 -0400

    remove power controller

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -5,10 +5,6 @@ fi
 
 echo "Building core..."
 yarn workspace @lightshow/core build
-echo "Building sms..."
-yarn workspace @lightshow/sms build
-echo "Building preview..."
-yarn workspace @lightshow/preview build
 echo "Building gpio-client..."
 yarn workspace @lightshow/gpio-client build
 echo "Building serial-dimmer-client..."

30a2c0fa0ef707c8da5f755edca56cbcb6599078
Patrick Dillon <dillon76@gmail.com>
2022-10-09 13:51:20 -0400

    dimmer updates

--- packages/serial-dimmer-client/src/index.ts
+++ packages/serial-dimmer-client/src/index.ts
@@ -1,17 +1,18 @@
-import SerialPort from "serialport";
-import { io, Socket } from "socket.io-client";
+import SerialPort from 'serialport';
+import { io, Socket } from 'socket.io-client';
+import { IOEvent } from '@lightshow/core';
 import {
   pinMappings as defaultPinMappings,
   parsePinMappings,
-} from "./pinMappings";
-import { log } from "./logger";
+} from './pinMappings';
+import { log } from './logger';
 const {
   SERVER_URL,
   LOG_MESSAGES,
-  PORT_ADDRESS = "/dev/ttyUSB0",
-  BAUD_RATE = "256000",
-  DIMMABLE_RANGE = "",
-  DIMMABLE_DOWN_RANGE = "",
+  PORT_ADDRESS = '/dev/ttyUSB0',
+  BAUD_RATE = '256000',
+  DIMMABLE_RANGE = '',
+  DIMMABLE_DOWN_RANGE = '',
   DEVICE_NAME,
 } = process.env;
 
@@ -19,34 +20,30 @@ const port = new SerialPort(PORT_ADDRESS, { baudRate: parseInt(BAUD_RATE) });
 
 let pinMappings = defaultPinMappings;
 
-port.on("open", () => {
+port.on('open', () => {
   startUp();
 });
 
-port.on("data", (data) => {
-  console.log("Data:", data.toString("utf8"));
+port.on('data', (data) => {
+  console.log('Data:', data.toString('utf8'));
 });
 
 const minLevel = 85;
 const maxLevel = 35;
-const maxVelocity = 127;
 
-const thresholdRange = maxVelocity / (minLevel - maxLevel);
-const dimmableNotes = DIMMABLE_RANGE.split(",");
-const dimmableDownNotes = DIMMABLE_DOWN_RANGE.split(",");
-
-const velocityCache: { [note: string]: number } = {};
+const dimmableNotes = DIMMABLE_RANGE.split(',');
+const dimmableDownNotes = DIMMABLE_DOWN_RANGE.split(',');
 
 function startUp() {
   log(`Pins initialized:`, pinMappings);
 
   setTimeout(() => {
-    toggleAllPins("on");
+    toggleAllPins('on');
   }, 500);
 
   let socket: Socket;
   if (!SERVER_URL) {
-    throw new Error("SERVER_URL must be configured");
+    throw new Error('SERVER_URL must be configured');
   } else {
     socket = io(SERVER_URL);
   }
@@ -58,41 +55,41 @@ function convertToBinary(pinValues: number[]) {
   return pinValues
     .reduce((t, v) => t + Math.pow(2, v - 1), 0)
     .toString()
-    .padStart(3, "0");
+    .padStart(3, '0');
 }
 
 function getSum(pinValues: number[]) {
   return pinValues
     .reduce((t, v) => t + v, 0)
     .toString()
-    .padStart(3, "0");
+    .padStart(3, '0');
 }
 
-function toggleAllPins(mode: "on" | "off") {
+function toggleAllPins(mode: 'on' | 'off') {
   if (!pinMappings) {
-    throw new Error("No pins mapped. Please configure NOTE_PIN_MAPPINGS.");
+    throw new Error('No pins mapped. Please configure NOTE_PIN_MAPPINGS.');
   }
 
   const pinBits = getSum([...new Set(Object.values(pinMappings))]);
 
-  const endLevel = mode === "on" ? maxLevel : minLevel;
-  const startLevel = mode === "on" ? minLevel : maxLevel;
+  const endLevel = mode === 'on' ? maxLevel : minLevel;
+  const startLevel = mode === 'on' ? minLevel : maxLevel;
   const payload = `${pinBits}${startLevel}${endLevel}00`;
 
   port.write(`${payload}\n`);
 }
 
-function togglePin(pin: number, mode: "on" | "off") {}
+function togglePin(pin: number, mode: 'on' | 'off') {}
 
 function listenForNoteMessages(socket: Socket) {
   socket
-    .on("map-notes", (deviceName, mappings) => {
+    .on('map-notes', (deviceName, mappings) => {
       if (deviceName === DEVICE_NAME) {
         pinMappings = parsePinMappings(mappings);
       }
     })
-    .on("song-start", () => toggleAllPins("off"))
-    .on("note-on", (note, velocity, length, sameNotes, autoOff = 1) => {
+    .on(IOEvent.TrackStart, () => toggleAllPins('off'))
+    .on(IOEvent.NoteOn, (note, noteNumber, length, sameNotes, velocity) => {
       const notes = [note];
       if (sameNotes?.length) {
         notes.push(...sameNotes);
@@ -113,7 +110,10 @@ function listenForNoteMessages(socket: Socket) {
       let startLevel = minLevel;
       let endLevel = maxLevel;
 
-      if (dimmableDownNotes.includes(note)) {
+      const autoOff = velocity >= 80 ? 1 : 0;
+      const dimUp = velocity % 20 > 10;
+
+      if (!dimUp) {
         startLevel = maxLevel;
         endLevel = minLevel;
       }
@@ -123,7 +123,7 @@ function listenForNoteMessages(socket: Socket) {
       }`;
       port.write(`${payload}\n`);
     })
-    .on("note-off", (note) => {
+    .on(IOEvent.NoteOff, (note) => {
       const pin = pinMappings[note];
 
       if (!pin || dimmableNotes.includes(note)) {
@@ -131,19 +131,14 @@ function listenForNoteMessages(socket: Socket) {
       }
 
       const payload = `${getSum([pin])}${maxLevel}${minLevel}00`;
-
       port.write(`${payload}\n`);
     })
-    .on("midi:file-end", () => {
-      setTimeout(() => toggleAllPins("on"), 100);
-      pinMappings = defaultPinMappings;
-    })
-    .on("song-end", () => {
-      setTimeout(() => toggleAllPins("on"), 100);
+    .on(IOEvent.TrackEnd, () => {
+      setTimeout(() => toggleAllPins('on'), 100);
       pinMappings = defaultPinMappings;
     });
 
-  if (LOG_MESSAGES === "true") {
+  if (LOG_MESSAGES === 'true') {
     socket.onAny((...args) => {
       log(args);
     });

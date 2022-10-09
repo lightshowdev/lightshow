92562cd4ebeccc48c5fe2a227b14c99232f0f482
Patrick Dillon <dillon76@gmail.com>
2022-10-09 13:52:12 -0400

    diagnostics routing

---
+++ packages/server/src/routes/diagnostics.ts
@@ -0,0 +1,27 @@
+import Router from '@koa/router';
+import type { IOEvent } from '@lightshow/core';
+import type { Server as SocketIOServer } from 'socket.io';
+
+export const diagnosticsRouter = new Router();
+
+diagnosticsRouter.get('/diagnostics/io', async (ctx) => {
+  const { io }: { io: SocketIOServer } = ctx.state;
+  const { note, event, velocity, length, sameNotes } = ctx.query;
+
+  if (note) {
+    const notes = (note as string).split(',');
+    notes.forEach((n) => {
+      io.emit(
+        event as string,
+        n,
+        parseInt((velocity as string) || '0'),
+        length ? parseInt(length as string) : undefined,
+        sameNotes ? (sameNotes as string).split(',') : undefined
+      );
+    });
+  } else {
+    io.emit(event as string);
+  }
+
+  ctx.body = { event, note, velocity };
+});
--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -11,7 +11,7 @@ import { loadPlugins } from './loader';
 
 import { Playlist, Console, Logger, SMSConfig } from '@lightshow/core';
 
-import { playlistRouter, consoleRouter } from './routes';
+import { playlistRouter, consoleRouter, diagnosticsRouter } from './routes';
 
 const plugins = loadPlugins();
 
@@ -20,6 +20,7 @@ const {
   TRACKS_PATH = '../../config/tracks',
   ELEMENTS_PATH = '../../config/elements',
   PORT = '3000',
+  LOG_LEVELS = '*',
 } = process.env;
 
 (async () => {
@@ -42,7 +43,9 @@ const {
   const router = new Router();
   const server = http.createServer(app.callback());
   const io = new SocketIOServer(server);
-  const logger = new Logger({ level: '*' });
+  const logger = new Logger({
+    level: LOG_LEVELS !== '*' ? LOG_LEVELS.split(',') : '*',
+  });
 
   const trackConsole = new Console({ io, playlist, logger });
 
@@ -53,27 +56,6 @@ const {
     config: { provider: SMS_PROVIDER as SMSConfig['provider'] },
     console: trackConsole,
     logger,
-  });
-
-  router.get('/test-io', async (ctx) => {
-    const { note, event, velocity, length, sameNotes } = ctx.query;
-
-    if (note) {
-      const notes = (note as string).split(',');
-      notes.forEach((n) => {
-        io.emit(
-          event as string,
-          n,
-          parseInt((velocity as string) || '0'),
-          length ? parseInt(length as string) : undefined,
-          sameNotes ? (sameNotes as string).split(',') : undefined
-        );
-      });
-    } else {
-      io.emit(event as string);
-    }
-
-    ctx.body = { event, note, velocity };
   });
 
   if (smsController) {
@@ -99,6 +81,7 @@ const {
 
   playlistRouter.prefix('/api');
   consoleRouter.prefix('/api');
+  diagnosticsRouter.prefix('/api');
 
   const tracksServeHandler = serve(path.resolve(TRACKS_PATH));
   const elementsServeHandler = serve(path.resolve(ELEMENTS_PATH));
@@ -117,10 +100,12 @@ const {
       ctx.state.playlist = playlist;
       ctx.state.logger = logger;
       ctx.state.trackConsole = trackConsole;
+      ctx.state.io = io;
       await next();
     })
     .use(playlistRouter.routes())
     .use(consoleRouter.routes())
+    .use(diagnosticsRouter.routes())
     .use(async (ctx, next) => {
       if (ctx.path.startsWith('/audio')) {
         ctx.path = ctx.path.replace('/audio', '');
